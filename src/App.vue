101dda1e05e730a854f8eb457d18c23fd5c42506
beigid <beigid@seattleu.edu>
2018-08-19 20:56:49 -0700

    styled list items and website design and color

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-      <Navigation></Navigation>
+     <div>
+  <div id="mySidenav" class="sidenav">
+          <a href="javascript:void(0)" class="closebtn" id="closeNav">&times;</a>
+         <router-link to="/about">About</router-link>
+    <br><router-link to="/contact">Contact</router-link>
+        </div>
+        
+        <span style="font-size:35px;cursor:pointer; padding-left:10px; color:white;" id="openNav">&#9776;</span>
+  </div>
     <router-view/>
  
   </div>
@@ -9,12 +17,10 @@
 
 <script>
 import Music from './components/Music'
-import Navigation from './components/Navigation'
 
 export default {
   name: 'App',
   components: {
-    'Navigation': Navigation
   }
  
 }
--- src/components/Music.vue
+++ src/components/Music.vue
@@ -1,22 +1,22 @@
 <template>        
  <div class="Music">
 
-    <h1>iTunes Music Finder</h1>
+    <h1 class="grow">iTunes Music Finder</h1>
     
 <form v-on:submit.prevent="findmusic">
-      <input type="text" id="myInput" v-model="artist" placeholder="Search Favorite Artist.." title="Type in a name">
-<input type="submit" value="Submit"></p>
+      <input type="text" id="myInput" v-model="artist" placeholder="Search Favorite Artist.." title="Type in a name" style="padding-bottom: 5px; padding-top: 3px;">
+<input type="submit" value="Submit" class="buttonStyle"></p>
 </form>
 
-
+<div>
 <ul id="results">
-  <li v-for="result in results">
+  <li v-for="result in results" class="styledList">
     <span><img v-bind:src="result.artworkUrl100"></span>
     <div><span><a v-bind:href="result.trackViewUrl" target="_blank">{{result.trackCensoredName}}</a></span> -
    <br><span><a v-bind:href="result.artistViewUrl" target="_blank">{{ result.artistName }}</a></span></div>
  
 <div>
-<audio controls preload="none" style="width:480px;">
+<audio controls preload="none">
  <source  v-bind:src="result.previewUrl" />
  </audio>
  </div>
@@ -24,7 +24,8 @@
    
   </li>
 </ul>
-    </div>
+</div>
+  </div>
 </template>
 <script>
 import axios from "axios";
@@ -63,7 +64,19 @@ export default {
 h1,
 h2 {
   font-weight: normal;
+  font-size:4em;
+  color:white;
+  font-family: 'PT Sans', sans-serif;
+
 }
+
+h1:hover {
+  color:#5c63b8;
+}
+
+.grow { transition: all .2s ease-in-out; }
+.grow:hover { transform: scale(1.1); }
+
 ul {
   list-style-type: none;
   padding: 0;
@@ -71,8 +84,48 @@ ul {
 li {
   display: block;
   margin: 0 10px;
+  border: 2px solid white;
 }
 a {
   color: #010a06;
 }
+
+li.styledList {
+margin-left: 20%;
+margin-right: 20%;
+margin-bottom: 20px;
+margin-top:20px;
+background-color: rgba(246, 227, 244, 0.4);
+border-radius:15px;
+}
+
+audio {
+  width: 80%;
+}
+
+.buttonStyle {
+display: inline-block;
+    padding: 6px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.42857143;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    -ms-touch-action: manipulation;
+    touch-action: manipulation;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    background-image: none;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    color: #fff;
+    background-color: #5c63b8;
+    border-color: #5f63d8;
+}
+
 </style>
